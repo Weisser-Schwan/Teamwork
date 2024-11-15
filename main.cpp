c50fc7286737f136cc2e8aa9c21b6b5116c56ba7
Weisser-Schwan <clausewitz0313@gmail.com>
2024-11-15 21:04:13 +0100

    Reset helloworld.cc

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 
 // custom function that takes a string as an argument
 void print(std::string msg) {
+// write to stdout
 std::cout << msg << std::endl;
 }
 // main function is called at program start
 int main(int argc, char** argv) {
-// variable declaration and initialization
-    std::string greeting = "Hello out there!";
+    // variable declaration and initialization
+    std::string greeting = "Hello, world!";
     print(greeting);
-return 0;
+    return 0;
 }

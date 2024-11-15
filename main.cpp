e3939ddb2279bbacd207c161f5591334b0dadcac
Weisser-Schwan <clausewitz0313@gmail.com>
2024-11-15 19:25:23 +0100

    There will absolutely be a fault

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <string>
 // custom function that takes a string as an argument
-void print(std::string msg) {
-// write to stdout
-std::cout << msg << std::endl;
+void print(std::string msg) 
+{std::cout << msg << std::endl;
 }
 // main function is called at program start
 int main() {
 // variable declaration and initialization
-std::string greeting = "Hello, world!";
+std::string greeting = "Hello out there!";
 print(greeting);
 return 0;
 }

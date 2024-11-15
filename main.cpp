a4c34db651382dc602ba13e1eb166f5af6e426c7
Weisser-Schwan <clausewitz0313@gmail.com>
2024-11-15 21:00:59 +0100

    1&3

--- main.cpp
+++ main.cpp
@@ -3,13 +3,12 @@
 
 // custom function that takes a string as an argument
 void print(std::string msg) {
-// write to stdout
 std::cout << msg << std::endl;
 }
 // main function is called at program start
 int main(int argc, char** argv) {
 // variable declaration and initialization
-    std::string greeting = "Hello, world!";
+    std::string greeting = "Hello out there!";
     print(greeting);
 return 0;
 }

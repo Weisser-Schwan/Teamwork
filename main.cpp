7358f1060ac13798c6a0d757fcfe37ff5acc20e2
Weisser-Schwan <clausewitz0313@gmail.com>
2024-11-18 17:32:05 +0100

    [aufgabe4] change main.cpp into aufgabe4's request's form

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
+std::string get_name_from_terminal(){
+	std::string name;
+	std::cin>>name;	
+	return name;
 
+}
 // custom function that takes a string as an argument
 void print(std::string msg) {
 // write to stdout
@@ -9,7 +14,12 @@ std::cout << msg << std::endl;
 // main function is called at program start
 int main(int argc, char** argv) {
     // variable declaration and initialization
-    std::string greeting = "Hello,my name ist Xinxin";
+    std::cout<<"Please enter your name:";
+    std::string greeting = "Hello,";
+    std::string name;
+    name=get_name_from_terminal();
+    greeting+=name;
+    greeting+="!";
     print(greeting);
     return 0;
 }
